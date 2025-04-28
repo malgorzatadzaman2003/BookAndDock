de142e41ffc755fb97a676114859f2c7765f3ff2
Andrzej Babulewicz <01180658@pw.edu.pl>
2025-04-28 21:48:04 +0200

    docks somewhat working

--- BookAndDock/admin.py
+++ BookAndDock/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from BookAndDock.models import Guide, Comment
+from BookAndDock.models import Guide, Comment, Dock
 
 # Register your models here.
 admin.site.register(Guide)
 admin.site.register(Comment)
+admin.site.register(Dock)
